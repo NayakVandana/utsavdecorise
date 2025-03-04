6705b9f098d3ffbe4d076ac01ebde12b80ffa5d7
Vandana Nayak <vandana.nayak@binarysphere.com>
2025-03-04 16:34:29 +0530

    route changes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin/inquiries', function () {
+        return view('admin.inquiries');
+    })->name('admin.inquiries');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -22,6 +29,18 @@ Route::middleware(['auth'])->group(function () {
         return view('admin.inquiries');
     })->name('admin.inquiries');
 });
+
+Route::get('/service', function () {
+    return view('welcome');
+})->name('service');
+
+Route::get('/about', function () {
+    return view('welcome');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('welcome');
+})->name('contact');
 
 require __DIR__.'/auth.php';
 
